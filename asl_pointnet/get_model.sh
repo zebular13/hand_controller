446b1c2a9ad593819fee99beb5109f20f46cded4
AlbertaBeef <grouby177@gmail.com>
2025-07-19 15:26:58 +0000

    add dependencies: asl pointnet model.

---
+++ asl_pointnet/get_model.sh
@@ -0,0 +1,4 @@
+# Get models from github.com/AlbertaBeef/asl_mediapipe_pointnet
+wget https://github.com/AlbertaBeef/asl_mediapipe_pointnet/releases/download/pointnet_hands_version_2/point_net_1.pth
+
+
